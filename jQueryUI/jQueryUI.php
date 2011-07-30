78488c013de39a7b2a811495d1345c559c1160c3
Robert Munteanu <robert@lmn.ro>
2011-07-31 02:09:30 +0300

    Initial version of the jQuery UI plugin

---
+++ jQueryUI/jQueryUI.php
@@ -0,0 +1,52 @@
+<?php
+# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+
+# jQuery UI plugin for MantisBT is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, 
+# either version 2 of the License, or (at your option) any later version.
+#
+# jQuery UI plugin for MantisBT is distributed in the hope 
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jQuery UI plugin for MantisBT.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Provides access to the jQuery UI library as a MantisBT plugin.
+ */
+class jQueryUIPlugin extends MantisPlugin {
+
+	function register() {
+        $this->name = plugin_lang_get("title");
+        $this->description = plugin_lang_get("description");
+
+        $this->version = "1.8.14";
+		$this->requires = array(
+			'MantisCore' => '1.2.0',
+			'jQuery' => '1.3'
+		);
+
+        $this->author = "Robert Munteanu";
+        $this->contact = "robert@lmn.ro";
+	}
+
+	function hooks() {
+		return array(
+			'EVENT_LAYOUT_RESOURCES' => 'resources',
+		);
+	}
+
+	/**
+	 * Create the resource link to load the jQuery UI library.
+	 */
+	function resources( $p_event ) {
+		return '<script type="text/javascript" src="' . plugin_file( 'jquery-ui-min.js' ) . '"></script>'.
+			'<link rel="stylesheet" type="text/css" href="'. plugin_file('jquery-ui.css') .' "></link>';
+	}
+}
+
