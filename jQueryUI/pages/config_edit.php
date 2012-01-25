15bcbb1a25580a80c10117a30dfe651b3c50bd4d
Jacky Alcine <jackyalcine@gmail.com>
2012-01-25 07:55:52 -0500

    Added configuration pages.

---
+++ jQueryUI/pages/config_edit.php
@@ -0,0 +1,30 @@
+<?php
+# Mantis - a php based bugtracking system
+
+# Copyright (C) 2002 - 2008  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+
+# Mantis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mantis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
+
+auth_reauthenticate();
+access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+
+$f_use_local = gpc_get_bool( 'use_local' );
+
+if ( is_blank ( $f_use_local ) )
+	plugin_config_set( 'use_local' , ON );
+
+echo plugin_config_get( 'use_local' );
+
+print_successful_redirect( plugin_page( 'config', true ) );
+
