d287d6644a199467c5672f8df1209ecba5783859
Jacky Alciné <jacky.alcine@thesii.org>
2012-01-26 05:21:23 -0500

    Updated copyright.

--- jQueryUI/pages/config.php
+++ jQueryUI/pages/config.php
@@ -1,21 +1,19 @@
 <?php
-# Mantis - A PHP based bugtracking system
+# Copyright (c) 2012 Jacky Alcine <jacky.alcine@thesii.org>
 
-# Copyright (C) 2002 - 2008  Mantis Team  - mantisbt-dev@lists.sourceforge.net
-# Copyright (C) 2012  Jacky Alcine <jacky.alcine@thesii.org>
-
-# Mantis is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# jQuery UI plugin for MantisBT is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, 
+# either version 2 of the License, or (at your option) any later version.
 #
-# Mantis is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# jQuery UI plugin for MantisBT is distributed in the hope 
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
+# along with jQuery UI plugin for MantisBT.  
+# If not, see <http://www.gnu.org/licenses/>.
 
 auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
